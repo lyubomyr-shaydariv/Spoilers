da201eee02fa7af67960dda819431e43f81bdaa5
Lyubomyr Shaydariv <lyubomyr-shaydariv@users.noreply.github.com>
2021-05-25 21:49:14 +0300

    Made spoiler-ed content lazy-loaded

--- resources/mw.Spoilers.js
+++ resources/mw.Spoilers.js
@@ -15,7 +15,13 @@
 				hideMsg = $parent.data( 'hidetext' ) || mw.msg( 'spoilers_hide_default' );
 			$parent.data( 'shown', !shown );
 			$parent.children( '.spoilers-button' ).text( !shown ? hideMsg : showMsg );
-			$parent.children( '.spoilers-body' ).slideToggle();
+			var $body = $parent.children( '.spoilers-body' ).slideToggle();
+			var $comments = $body.contents().filter( function( e ) { return this.nodeType === 8; } );
+			if ( $comments.length ) {
+				var bodyHtml = $comments[0].nodeValue;
+				$body.html( bodyHtml );
+				$comment.remove();
+			}
 		} );
 	};
 
